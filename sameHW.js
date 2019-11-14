5b440d68303318c8ae56489d5838745e9cea73ad
wenxing <xyb851@126.com>
2019-11-14 13:51:55 +0800

    增加：1、元素顶边分组等高 ；2、元素左边分组等宽 ；3、元素顶边分组等高、左边分组等宽

--- sameHW.js
+++ sameHW.js
@@ -1,23 +1,40 @@
-// 页面元素等宽高
-// 创建日期 2019-10-27
-// 版本：0.1
-// 版权：问星
-
-//	使用方式：
-// 引入本库文件：<script src="sameHW.js" type="text/javascript"></script>
-//	sameHW.setSameHWTage("*","a",1);	//所有class为a的元素等高
-//	sameHW.setSameHWTage("*","a",2);	//所有class为a的元素等宽
-//	sameHW.setSameHWTage("*","a",0);	//所有class为a的元素等宽高
-//	sameHW.setSameHWClass("b", "a", 1); //class为b的元素中所有class为a的子元素等高
-//	sameHW.setSameHWClass("b", "a", 2); //class为b的元素中所有class为a的子元素等宽
-//	sameHW.setSameHWClass("b", "a", 0); //class为b的元素中所有class为a的子元素等宽高
-//	sameHW.setSameHWId("c", "a", 1); //ID为c的元素中所有class为a的子元素等高
-//	sameHW.setSameHWId("c", "a", 2); //ID为c的元素中所有class为a的子元素等宽
-//	sameHW.setSameHWId("c", "a", 0); //ID为c的元素中所有class为a的子元素等宽高
+/* 	页面元素等宽高
+创建日期 2019-10-27
+版本：0.2
+版权：问星
+修改：
+	2019-11-14
+		新增:
+			1、元素顶边分组等高
+			2、元素左边分组等宽
+			3、元素顶边分组等高、左边分组等宽
+	使用方式：
+引入本库文件：<script src="sameHW.js" type="text/javascript"></script>
+	sameHW.setSameHWTage("*","a",1);	//所有class为a的元素等高
+	sameHW.setSameHWTage("*","a",2);	//所有class为a的元素等宽
+	sameHW.setSameHWTage("*","a",3);	//所有class为a的元素等宽高
+	sameHW.setSameHWTage("*","a",4);	//所有class为a的元素顶边分组等高
+	sameHW.setSameHWTage("*","a",5);	//所有class为a的元素左边分组等宽
+	sameHW.setSameHWTage("*","a",6);	//所有class为a的元素顶边分组等高、左边分组等宽
+	sameHW.setSameHWClass("b", "a", 1); //class为b的元素中所有class为a的子元素等高
+	sameHW.setSameHWClass("b", "a", 2); //class为b的元素中所有class为a的子元素等宽
+	sameHW.setSameHWClass("b", "a", 3); //class为b的元素中所有class为a的子元素等宽高
+	sameHW.setSameHWClass("b", "a", 4); //class为b的元素中所有class为a的子元素顶边分组等高
+	sameHW.setSameHWClass("b", "a", 5); //class为b的元素中所有class为a的子元素左边分组等宽
+	sameHW.setSameHWClass("b", "a", 6); //class为b的元素中所有class为a的子元素顶边分组等高、左边分组等宽
+	sameHW.setSameHWId("c", "a", 1); //ID为c的元素中所有class为a的子元素等高
+	sameHW.setSameHWId("c", "a", 2); //ID为c的元素中所有class为a的子元素等宽
+	sameHW.setSameHWId("c", "a", 3); //ID为c的元素中所有class为a的子元素等宽高
+	sameHW.setSameHWId("c", "a", 4); //ID为c的元素中所有class为a的子元素顶边分组等高
+	sameHW.setSameHWId("c", "a", 5); //ID为c的元素中所有class为a的子元素左边分组等宽
+	sameHW.setSameHWId("c", "a", 6); //ID为c的元素中所有class为a的子元素顶边分组等高、左边分组等宽
+*/
 
 var sameHW = {
 	maxHeight: 0,
 	maxWidth: 0,
+	maxHeights: [], //元素最大高度组
+	maxWidths: [], //元素最大宽度组
 	//获取元素所有的子元素class为name的元素
 	getAllSubElement: function(elem, name) {
 		var s = elem.children;
@@ -29,11 +46,87 @@ var sameHW = {
 				if (s[i].getAttribute("class") == name) //判断和需要CLASS名字相同的，并组成一个数组 
 				{
 					sClass[j] = s[i];
+					console.log(sClass[j])
 					j++;
 				}
 			}
 		}
 		return sClass;
+	},
+	//元素按左边位置分组
+	getLeftGroup: function(els) {
+		var rels = [];
+		var lefts = [];
+		//console.log(els)
+		for (var i = 0; i < els.length; i++) {
+			if (i == 0) {
+				lefts.push(els[i].offsetLeft);
+				var ar = [];
+				ar.push(els[i]);
+				rels.push(ar);
+			} else {
+				var a = 0;
+				for (var j = 0; j < lefts.length; j++) {
+					if (els[i].offsetLeft == lefts[j]) {
+						rels[j].push(els[i]);
+						a = 1;
+						break;
+					}
+				}
+				if (a == 0) {
+					lefts.push(els[i].offsetLeft);
+					var ar = [];
+					ar.push(els[i]);
+					rels.push(ar);
+				}
+			}
+		}
+		return rels;
+	},
+	//分组取最大宽度
+	getMaxWInGroup: function(gele) {
+		maxWidths = [];
+		for (var i = 0; i < gele.length; i++) {
+			this.getMaxWidth(gele[i], gele[i].length)
+			maxWidths.push(this.maxWidth);
+		}
+	},
+	//元素按顶边位置分组
+	getTopGroup: function(els) {
+		var rels = [];
+		var tops = [];
+		for (var i = 0; i < els.length; i++) {
+			if (i == 0) {
+				tops.push(els[i].offsetTop);
+				var ar = [];
+				ar.push(els[i]);
+				rels.push(ar);
+			} else {
+				var a = 0;
+				for (var j = 0; j < tops.length; j++) {
+					if (els[i].offsetTop == tops[j]) {
+						rels[j].push(els[i]);
+						a = 1;
+						break;
+					}
+				}
+				if (a == 0) {
+					tops.push(els[i].offsetTop);
+					var ar = [];
+					ar.push(els[i]);
+					rels.push(ar);
+				}
+			}
+		}
+		return rels;
+	},
+	//分组取最大高度
+	getMaxHInGroup: function(gele) {
+		maxHeights = [];
+		for (var i = 0; i < gele.length; i++) {
+			this.getMaxHeight(gele[i], gele[i].length)
+			maxHeights.push(this.maxHeight);
+		}
 	},
 	//取最大高度
 	getMaxHeight: function(els, l) {
@@ -51,8 +144,8 @@ var sameHW = {
 	getMaxWidth: function(els, l) {
 		var s = 'Math.max(';
 		for (var i = 0; i < l; i++) {
-			if (i == l - 1) {
-				s += els[i].scrollWidth + ');';
+			if (i == (l - 1)) {
+				s += els[i].scrollWidth + ')';
 			} else {
 				s += els[i].scrollWidth + ',';
 			}
@@ -74,6 +167,30 @@ var sameHW = {
 		}
 		this.maxWidth = eval(s);
 		this.maxHeight = eval(s1);
+	},
+	//分组等高
+	setsameHg: function(els) {
+		var gels = this.getTopGroup(els);
+		this.getMaxHInGroup(gels);
+		//console.log(JSON.stringify(maxHeights))
+		for (var i = 0; i < gels.length; i++) {
+			for (var j = 0; j < gels[i].length; j++) {
+				var el = gels[i][j];
+				el.style.height = maxHeights[i] + 'px';
+			}
+		}
+	},
+	//分组等宽
+	setsameWg: function(els) {
+		var gels = this.getLeftGroup(els);
+		this.getMaxWInGroup(gels);
+		//console.log(JSON.stringify(maxHeights))
+		for (var i = 0; i < gels.length; i++) {
+			for (var j = 0; j < gels[i].length; j++) {
+				var el = gels[i][j];
+				el.style.width = maxWidths[i] + 'px';
+			}
+		}
 	},
 	//等高
 	setsameH: function(els) {
@@ -97,18 +214,7 @@ var sameHW = {
 			els[i].style.width = this.maxWidth + 'px';
 		}
 	},
-	//获取元素最大高度或宽度
-	//type 0：取紧了大宽高；1:取最大高度；2取最大宽度； 
-	getElemenMaxHW: function(els, type) {
-		var l = els.length;
-		if (type == 1) {
-			this.getMaxHeight(els, l);
-		} else if (type == 2) {
-			this.getMaxWidth(els, l);
-		} else {
-			this.getMaxWH(els, l);
-		}
-	},
+
 	//取标签tage中class名为name的元素，返回元素数组
 	getTageClass: function(tage, name) {
 		var classobj = new Array(); //定义数组 
@@ -138,62 +244,72 @@ var sameHW = {
 	getClassInSub: function(father, subclass) {
 		var fobj = this.getClass(father);
 		var l = fobj.length;
-		var j=0;
+		var j = 0;
 		var robj = [];
 		for (var i = 0; i < l; i++) {
 			var sobj = this.getAllSubElement(fobj[i], subclass);
-			for(var k=0;k<sobj.length;k++){
-				robj[j]=sobj[k];
+			for (var k = 0; k < sobj.length; k++) {
+				robj[j] = sobj[k];
 				j++;
 			}
 		}
 		return robj;
 	},
+
+	//条件设置等宽高
+	setSame: function(eobj, type) {
+		var l = eobj.length;
+		switch (type) {
+			case 1: //所有等高
+				this.getMaxHeight(eobj, l);
+				this.setsameH(eobj);
+				break;
+			case 2: //所有等宽
+				this.getMaxWidth(eobj, l);
+				this.setsameW(eobj);
+				break;
+			case 3: //所有等宽高
+				this.getMaxWH(eobj, l);
+				this.setsameHW(eobj);
+				break;
+			case 4: //顶边分组等高
+				this.setsameHg(eobj);
+				break;
+			case 5: //左边分组等宽
+				this.setsameWg(eobj);
+				break;
+			case 6: //顶边分组等高；左边分组等宽
+				this.setsameHg(eobj);
+				this.setsameWg(eobj);
+				break;
+			default:
+				break;
+		}
+	},
 	//设置指定class中的子class等宽高
 	//father 父class名
 	//sub 子class名
-	//type 0：等宽高；1:等高；2：等宽； 
+	//type 0：等宽高；1:等高；2：等宽；3:元素顶边分组等高；4：元素左边分组等宽；5：元素顶边分组等高，元素左边分组等宽 
 	setSameHWClass: function(father, sub, type) {
-		
 		var eobj = this.getClassInSub(father, sub);
-		this.getElemenMaxHW(eobj, type);
-		if (type == 1) {
-			this.setsameH(eobj);
-		} else if (type == 2) {
-			this.setsameW(eobj);
-		} else {
-			this.setsameHW(eobj);
-		}
+		this.setSame(eobj, type);
 	},
 	//设置指定ID中的子class等宽高
 	//father 父id名
 	//sub 子class名
-	//type 0：等宽高；1:等高；2：等宽；  
+	//type 0：等宽高；1:等高；2：等宽；3:元素顶边分组等高；4：元素左边分组等宽；5：元素顶边分组等高，元素左边分组等宽
 	setSameHWId: function(id, sub, type) {
-		eid=document.getElementById(id);
+		var eid = document.getElementById(id);
 		var eobj = this.getAllSubElement(eid, sub);
-		this.getElemenMaxHW(eobj, type);
-		if (type == 1) {
-			this.setsameH(eobj);
-		} else if (type == 2) {
-			this.setsameW(eobj);
-		} else {
-			this.setsameHW(eobj);
-		}
+		this.setSame(eobj, type);
 	},
 	//设置指定tage中的子class等宽高
 	//father 父id名
 	//sub 子class名
-	//type 0：等宽高；1:等高；2：等宽； 
+	//type 0：等宽高；1:等高；2：等宽；3:元素顶边分组等高；4：元素左边分组等宽；5：元素顶边分组等高，元素左边分组等宽
 	setSameHWTage: function(tage, sub, type) {
 		var eobj = this.getTageClass(tage, sub);
-		this.getElemenMaxHW(eobj, type);
-		if (type == 1) {
-			this.setsameH(eobj);
-		} else if (type == 2) {
-			this.setsameW(eobj);
-		} else {
-			this.setsameHW(eobj);
-		}
+		this.setSame(eobj, type);
+
 	}
 }
